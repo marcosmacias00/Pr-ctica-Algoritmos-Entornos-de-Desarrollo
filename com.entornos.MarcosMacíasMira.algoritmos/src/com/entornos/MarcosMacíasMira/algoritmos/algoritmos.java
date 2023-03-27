81ff68330321f5951216d3a3f4959312bf86a915
Marcos <mmm0084@alu.medac.es>
2023-03-27 10:40:51 +0200

    Metodo Fibonacci

--- com.entornos.MarcosMacíasMira.algoritmos/src/com/entornos/MarcosMacíasMira/algoritmos/algoritmos.java
+++ com.entornos.MarcosMacíasMira.algoritmos/src/com/entornos/MarcosMacíasMira/algoritmos/algoritmos.java
@@ -1,5 +1,27 @@
 package com.entornos.MarcosMacíasMira.algoritmos;
-
+/**
+ * 
+ * 
+ * @author Marcos
+ * Version 1.0
+ * Esta clase sirve para calcular números fibonacci
+ */
 public abstract class algoritmos {
-
+	/**
+	 * 
+	 * Recibe un numero entero y devuelve un numero fibonacci
+	 * @param posicion 
+	 * @return numero fibonacci del numero entero
+	 */
+	public static int fibonacci(int posicion) {
+		int siguiente = 1, actual = 0, temporal = 0;
+		for (int y = 1; y <= posicion; y++) {
+			// Si no quieres imprimirla, comenta la siguiente línea:
+			temporal = actual;
+			actual = siguiente;
+			siguiente = siguiente + temporal;
+		}
+		// Si no quieres imprimirla, comenta la siguiente línea:
+		return actual;
+	}
 }
